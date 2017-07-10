870b565e88886c03c8c46f400d9c717ff6b1fb70
Henry <henry@alturasoluciones.com>
2017-07-10 17:17:32 -0500

    Fixing Rspec for Users Model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { FactoryGirl.build(:user) }
 
+  describe 'validations' do
+    subject { FactoryGirl.create(:user) }
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
-
-    # fails with the custom validation as it checks for a case when birthday is nil
-    # it { should validate_presence_of(:birthday) }
+    it { should validate_presence_of(:birthday) }
     it { should validate_presence_of(:email) }
-
-    it "user cannot have birthday later than today" do
-      user = FactoryGirl.build(:user)
-      expect(user).to be_valid
-
-      user.birthday = Time.now + 1.year
-      expect(user).to_not be_valid
-    end
+  end
+  
+  it "user cannot have birthday later than today" do
+    user = FactoryGirl.build(:user)
+    expect(user).to be_valid
+    user.birthday = Time.now + 1.year
+    expect(user).to_not be_valid
+  end
 end
